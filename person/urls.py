468c37c9301fb9fed2e18a6131992c1f08203506
JDKahle <112479319+JDKahle@users.noreply.github.com>
2022-10-09 12:02:44 +0200

    .

--- person/urls.py
+++ person/urls.py
@@ -3,6 +3,8 @@ from .views import PersonHomeView
 
 from . import views
 
+app_name = 'person'
+
 urlpatterns = [
     #path('', views.simple_view),
     path('', PersonHomeView.as_view(), name='home'),
